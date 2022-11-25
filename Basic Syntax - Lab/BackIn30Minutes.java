fa52dff3dae62f6925dd72d06c7e632a561125e8
mfilipova31 <mfilipova31@gmail.com>
2022-11-25 14:17:38 +0200

    Adding task from Lab
    Problem Solution for Basic Syntax - Lab

---
+++ Basic Syntax - Lab/BackIn30Minutes.java
@@ -0,0 +1,26 @@
+package BasicSyntaxConditionalsLoops.Lab;
+
+import java.util.Scanner;
+
+public class BackIn30Minutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hours = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine()) + 30;
+
+        if (minutes > 59){
+            hours ++;
+            minutes -= 60;
+        }
+
+        if (hours > 23){
+            hours = 0;
+        }
+
+        if (minutes < 10){
+            System.out.printf("%d:%02d\n", hours, minutes);
+        } else {
+            System.out.printf("%d:%d", hours, minutes);
+        }
+    }
+}
