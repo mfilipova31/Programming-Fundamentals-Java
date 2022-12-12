49a1206e6515a8fe3739ea89cfa98f56f8c62497
mfilipova31 <mfilipova31@gmail.com>
2022-12-12 19:38:20 +0200

    Added tasks from Lab
    Problem solutions for Data Types and Variables - Lab.

---
+++ Data Types Variables - Lab/ExactSumRealNumbers.java
@@ -0,0 +1,21 @@
+package DataTypesAndVariables.Lab;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ExactSumRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        BigDecimal sum = new BigDecimal(0);
+
+        for (int i = 1; i <= n; i++) {
+            BigDecimal currentNumber = new BigDecimal(scanner.nextLine());
+
+            sum = sum.add(currentNumber);
+        }
+
+        System.out.println(sum);
+
+    }
+}
---
+++ Data Types Variables - Lab/MetersToKilometers.java
@@ -0,0 +1,14 @@
+package DataTypesAndVariables.Lab;
+
+import java.util.Scanner;
+
+public class MetersToKilometers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        short meters = Short.parseShort(scanner.nextLine());
+        double kilometers = meters / 1000.0;
+
+        System.out.printf("%.2f", kilometers);
+    }
+}
